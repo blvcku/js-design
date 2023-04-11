531e4f4ac78e61ae63abf0b8463207a73bb30fb0
Piotr Orzechowski <blvck2005@gmail.com>
2023-04-11 02:46:13 +0200

    fix(project-page): colors component styles

--- src/features/project-template/components/api-components/colors/Colors.styles.ts
+++ src/features/project-template/components/api-components/colors/Colors.styles.ts
@@ -3,7 +3,7 @@ import { media, pixelsToRem } from '@/styles/Mixins';
 import { ColorsContainerProps, ColorBoxProps } from './Colors.types';
 import isNumber from '@/helpers/isNumber';
 
-export const ColorsContainer = styled.section<ColorsContainerProps>`
+export const ColorsContainer = styled.ul<ColorsContainerProps>`
     margin-top: ${({ marginTop }) =>
         isNumber(marginTop) ? pixelsToRem(marginTop) : `4rem`};
     display: grid;
